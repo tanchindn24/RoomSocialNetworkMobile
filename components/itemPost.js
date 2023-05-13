72fa6f897b87fe45c0974e63ec21b399e4ce8002
newuserrname <phamtruong24042001@gmail.com>
2023-05-14 00:03:45 +0700

    format time ago

--- components/itemPost.js
+++ components/itemPost.js
@@ -6,6 +6,60 @@ import axios from "axios";
 
 function itemPost(props) {
 
+    function formatTime(time) {
+        switch (typeof time) {
+            case 'number':
+                break;
+            case 'string':
+                time = +new Date(time);
+                break;
+            case 'object':
+                if (time.constructor === Date) time = time.getTime();
+                break;
+            default:
+                time = +new Date();
+        }
+        const time_formats = [
+            [60, 'seconds', 1], // 60
+            [120, '1 minute ago', '1 minute from now'], // 60*2
+            [3600, 'minutes', 60], // 60*60, 60
+            [7200, '1 hour ago', '1 hour from now'], // 60*60*2
+            [86400, 'hours', 3600], // 60*60*24, 60*60
+            [172800, 'Yesterday', 'Tomorrow'], // 60*60*24*2
+            [604800, 'days', 86400], // 60*60*24*7, 60*60*24
+            [1209600, 'Last week', 'Next week'], // 60*60*24*7*4*2
+            [2419200, 'weeks', 604800], // 60*60*24*7*4, 60*60*24*7
+            [4838400, 'Last month', 'Next month'], // 60*60*24*7*4*2
+            [29030400, 'months', 2419200], // 60*60*24*7*4*12, 60*60*24*7*4
+            [58060800, 'Last year', 'Next year'], // 60*60*24*7*4*12*2
+            [2903040000, 'years', 29030400], // 60*60*24*7*4*12*100, 60*60*24*7*4*12
+            [5806080000, 'Last century', 'Next century'], // 60*60*24*7*4*12*100*2
+            [58060800000, 'centuries', 2903040000] // 60*60*24*7*4*12*100*20, 60*60*24*7*4*12*100
+        ];
+        let seconds = (+new Date() - time) / 1000,
+            token = 'ago',
+            list_choice = 1;
+
+        if (seconds === 0) {
+            return 'Just now'
+        }
+        if (seconds < 0) {
+            seconds = Math.abs(seconds);
+            token = 'from now';
+            list_choice = 2;
+        }
+        let i = 0,
+            format;
+        while (format = time_formats[i++])
+            if (seconds < format[0]) {
+                if (typeof format[2] == 'string')
+                    return format[list_choice];
+                else
+                    return Math.floor(seconds / format[2]) + ' ' + format[1] + ' ' + token;
+            }
+        return time;
+    }
+
     const [posts, setPosts] = useState(null)
 
     const host = 'http://192.168.1.5:2023';
@@ -98,17 +152,6 @@ function itemPost(props) {
                                         />
                                         <Text style={styles.textPost}>{item.view}</Text>
                                     </View>
-                                    {/*<View style={{*/}
-                                    {/*    flexDirection: 'row',*/}
-                                    {/*    justifyContent: 'space-between'*/}
-                                    {/*}}>*/}
-                                    {/*    <Icon name={'money'}*/}
-                                    {/*          color={'gray'}*/}
-                                    {/*          size={18}*/}
-                                    {/*    />*/}
-                                    {/*    <Text*/}
-                                    {/*        style={styles.textPost}>{((item.price).length > 7) ? (((item.price).substring(0, 7 - 3)) + '...') : item.price} đ</Text>*/}
-                                    {/*</View>*/}
                                     <View style={{
                                         flexDirection: 'row',
                                         justifyContent: 'space-between'
@@ -118,6 +161,17 @@ function itemPost(props) {
                                               size={20}
                                         />
                                         <Text style={styles.textPost}>{item.area} m2</Text>
+                                    </View>
+                                    <View style={{
+                                        flexDirection: 'row',
+                                        justifyContent: 'space-between'
+                                    }}>
+                                        <Icon name={'clock-o'}
+                                              color={'gray'}
+                                              size={18}
+                                        />
+                                        <Text
+                                            style={styles.textPost}>{formatTime(new Date(item.created_at))}</Text>
                                     </View>
                                 </View>
                             </View>
